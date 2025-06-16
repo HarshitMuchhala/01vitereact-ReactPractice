4c2f06e43077ff27e14aca721387bd56d87d042f
Harshit Muchhala <harshitmuchhala@gmail.com>
2025-06-17 04:05:19 +0530

    01vitereact ReactPractice

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+function App() {
+  const userName="Harshit";
+  return (
+    <>
+      <Chai></Chai>
+      <h1>chai aur react with vite {userName}</h1> 
+      <p> test para</p>
+    </>
+  );
+} 
+
+export default App;
